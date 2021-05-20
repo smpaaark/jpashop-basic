b40f5d8d001a96881a22d753493193f9e8f1a9e5
smpark1020 <smpark911020@gmail.com>
2021-05-20 23:50:51 +0900

    실전 예제 1 - 요구사항 분석과 기본 매핑(ing)

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+}
